dbc6006ab5980db34e957a8f46ed99651f5efaeb
cmullins77 <mullins.cassie@gmail.com>
2017-09-12 20:28:48 -0400

    Implemented Undo

--- Cassie/index.js
+++ Cassie/index.js
@@ -2,6 +2,8 @@ var buttonExists = true;
 var shapes = ["a-box", "a-sphere", "a-circle", "a-cone", "a-plane", "a-ring", "a-torus", "a-torus-knot", "a-triangle"];
 var shapeNames = ["box", "sphere", "circle", "cone", "plane", "ring", "torus", "torusKnot", "triangle"];
 var itemNum = 0;
+var models = ["obj: #pokemon-obj; mtl: #pokemon-mtl"];
+var changes = [];
 
 function init() {
     document.querySelector("a-scene").removeChild(document.getElementsByClassName("a-enter-vr")[0]);
@@ -12,7 +14,7 @@ function init() {
 //Handles Summon Button Press
 function summonButtonPress() {
     removeSummmon();
-    createShapeButtons();
+    createAddButtons();
     if (buttonExists) {
       init();
     }
@@ -27,6 +29,96 @@ function shapeButtonPress(i) {
 //Removes the Summon Button
 function removeSummmon() {
     document.querySelector("center").removeChild(document.getElementsByClassName("buttonSummon")[0]);
+}
+//Create Add Buttons
+function createAddButtons() {
+  var table = document.createElement("table");
+  var row = document.createElement("tr");
+  var body = document.querySelector("center");
+
+  //Models Button
+  var button = document.createElement("button");
+  button.setAttribute("onclick", "modelButtonPress1()");
+  button.setAttribute("type", "button");
+  button.setAttribute("class", "optionButton");
+  button.innerText = "Models";
+  var cell = document.createElement("td");
+  cell.appendChild(button);
+  row.appendChild(cell);
+
+  //Shapes Button
+  var button = document.createElement("button");
+  button.setAttribute("onclick", "shapeButtonPress1()");
+  button.setAttribute("type", "button");
+  button.setAttribute("class", "optionButton");
+  button.innerText = "Shapes";
+  var cell = document.createElement("td");
+  cell.appendChild(button);
+  row.appendChild(cell);
+
+  table.appendChild(row);
+  table.setAttribute("id", "buttonTable");
+  body.appendChild(table);
+}
+
+function modelButtonPress1() {
+  removeButtons();
+  createModelButtons();
+}
+
+function shapeButtonPress1() {
+  removeButtons();
+  createShapeButtons();
+}
+
+//Adds Model Buttons
+function createModelButtons() {
+  var table = document.createElement("table");
+  var row = document.createElement("tr");
+  var body = document.querySelector("center");
+
+  //Models Button
+  var button = document.createElement("button");
+  button.setAttribute("onclick", "modelButtonPress(0)");
+  button.setAttribute("type", "button");
+  button.setAttribute("class", "optionButton");
+  button.innerText = "Pokeball";
+  var cell = document.createElement("td");
+  cell.appendChild(button);
+  row.appendChild(cell);
+
+  //Shapes Button
+  var button = document.createElement("button");
+  button.setAttribute("onclick", "addNewModelButtonPress()");
+  button.setAttribute("type", "button");
+  button.setAttribute("class", "optionButton");
+  button.innerText = "Add New Model";
+  var cell = document.createElement("td");
+  cell.appendChild(button);
+  row.appendChild(cell);
+
+  table.appendChild(row);
+  table.setAttribute("id", "buttonTable");
+  body.appendChild(table);
+}
+
+function modelButtonPress(i) {
+  removeButtons();
+  createOptions();
+  displayModel(i);
+}
+
+function displayModel(i) {
+  var scene = document.querySelector("a-scene");
+  //creates model
+  var model = document.createElement("a-entity");
+  model.setAttribute("id", "item");
+  model.setAttribute("class", "model");
+  model.setAttribute("obj-model", models[i]);
+  model.setAttribute('position', '0 1.25 -5');
+  model.setAttribute("rotation", "-90 0 0");
+  model.setAttribute("scale", "1 1 1");
+  scene.appendChild(model);
 }
 
 //Adds Shape Buttons
@@ -87,12 +179,12 @@ function createShapeButtons() {
   cell.appendChild(button);
   row2.appendChild(cell);
 
-  //Ring Button
+  //Model Button
   var button = document.createElement("button");
   button.setAttribute("onclick", "shapeButtonPress(5)");
   button.setAttribute("type", "button");
   button.setAttribute("class", "optionButton");
-  button.innerText = "Ring";
+  button.innerText = "Model";
   var cell = document.createElement("td");
   cell.appendChild(button);
   row2.appendChild(cell);
@@ -209,13 +301,6 @@ function display(i) {
     item.setAttribute('VertexC', "-1 2 -5.5")
     item.setAttribute('color', "#0000FF");
     scene.appendChild(item);
-    //creates model
-    /*var model = document.createElement("a-entity");
-    scene.appendChild(model);
-    model.id = 'sphere';
-    model.setAttribute("obj-model", "obj: #pokemon-obj; mtl: #pokemon-mtl");
-    model.setAttribute('position', '0 1.25 -5');
-    model.setAttribute("rotation", "-90 0 0");*/
 }
 
 //2)Remove Edit, Undo, Delete, Finish
@@ -264,8 +349,18 @@ function disappear() {
 
 //4)Undo
 //Undo most recent edit
-function undo() {
+function undoButtonPress() {
   console.log("Undo!");
+  if (changes.length > 0) {
+    var lastMove = changes.pop(changes.length - 1);
+    if (lastMove[0] === "move") {
+      moveSphere(lastMove[1], lastMove[2], true);
+    } else if (lastMove[0] === "rotate") {
+      rotateSphere(lastMove[1], true);
+    } else if (lastMove[0] === "size") {
+      resizeSphere(lastMove[1], true);
+    }
+  }
 }
 
 //5) Add: Move, Rotate, Size, Color, Finish, Remove: Edit, Delete, Undo, Finish
@@ -353,7 +448,7 @@ function moveButtonPress() {
 function createButton(axis, direction) {
   var button = document.createElement("button");
 
-  button.setAttribute("onClick", "moveSphere('" + axis + "', '" + direction + "')");
+  button.setAttribute("onClick", "moveSphere('" + axis + "', '" + direction + "', false)");
   button.setAttribute("class", "button");
   button.innerText = direction == 'up' ? axis.toUpperCase() + "+" : axis.toUpperCase() + "-";
 
@@ -406,7 +501,7 @@ function createMoveButtons() {
  * @param {String} axis - 'x' or 'y'
  * @param {String} direction - 'up' or 'down'
 */
-function moveSphere(axis, direction) {
+function moveSphere(axis, direction, undoing) {
   var s = document.getElementById("item");
   var scene = document.querySelector("a-scene");
 
@@ -418,12 +513,33 @@ function moveSphere(axis, direction) {
   switch (axis) {
     case "x":
       s.setAttribute('position', { x: direction === 'up' ? _x + 1 : _x - 1, y: _y, z: _z });
+      if (!undoing) {
+        if (direction === 'up') {
+          changes.push(["move", "x", "down"]);
+        } else {
+          changes.push(["move", "x", "up"]);
+        }
+      }
       break;
     case "y":
       s.setAttribute('position', { x: _x, y: direction === 'up' ? _y + 1 : _y - 1, z: _z });
+      if (!undoing) {
+        if (direction === 'up') {
+          changes.push(["move", "y", "down"]);
+        } else {
+          changes.push(["move", "y", "up"]);
+        }
+      }
       break;
     case "z":
       s.setAttribute('position', { x: _x, y: _y, z: direction === 'up' ? _z + 1 : _z - 1 });
+      if (!undoing) {
+        if (direction === 'up') {
+          changes.push(["move", "z", "down"]);
+        } else {
+          changes.push(["move", "z", "up"]);
+        }
+      }
       break;
   }
 }
@@ -454,7 +570,7 @@ function createRotateButtons() {
     body.appendChild(button);
 
     var button = document.createElement("button");
-    button.setAttribute("onClick", "rotateSphere(-5)");
+    button.setAttribute("onClick", "rotateSphere(-5, false)");
     button.setAttribute("class", "button");
     button.innerText = "<-";
     var cell = document.createElement("td");
@@ -462,7 +578,7 @@ function createRotateButtons() {
     row.appendChild(cell);
 
     var button = document.createElement("button");
-    button.setAttribute("onClick", "rotateSphere(5)");
+    button.setAttribute("onClick", "rotateSphere(5, false)");
     button.setAttribute("class", "button");
     button.innerText = "->";
     var cell = document.createElement("td");
@@ -478,7 +594,7 @@ function createRotateButtons() {
  * rotates the entity
  * @param {int} degrees turned
 */
-function rotateSphere(degrees) {
+function rotateSphere(degrees, undoing) {
     var s = document.getElementById("item");
     var scene = document.querySelector("a-scene");
 
@@ -487,6 +603,10 @@ function rotateSphere(degrees) {
     var _x = s.getAttribute('rotation').x;
     var _z = s.getAttribute('rotation').z;
     s.setAttribute('rotation', { x: _x, y: _y + degrees, z: _z });
+
+    if (!undoing) {
+      changes.push(["rotate", degrees * -1]);
+    }
 }
 
 //10)Add Move, Rotate, Size, Color, Finish, Remove: <-, ->, Finish
@@ -563,7 +683,7 @@ function createSizeButtons() {
  * resizes the entity
  * @param {int} amount changed
  */
-function resizeSphere(change) {
+function resizeSphere(change, undoing) {
     var s = document.getElementById("item");
     var scene = document.querySelector("a-scene");
     var type = s.getAttribute("class");
@@ -670,6 +790,19 @@ function resizeSphere(change) {
         _z = _z / 1.1;
       }
       s.setAttribute('VertexC', {x:_x, y: _y, z: _z});
+    } else if (type === "model") {
+      var sX = parseFloat(s.getAttribute("scale").x);
+      var sY = parseFloat(s.getAttribute("scale").y);
+      var sZ = parseFloat(s.getAttribute("scale").z);
+      var sX_change = sX + change;
+      var sY_change = sY + change;
+      var sZ_change = sZ + change;
+      if (!(sX_change <= 0) && !(sY_change <= 0) && !(sZ_change <= 0)) {
+        s.setAttribute("scale", {x: sX_change, y: sY_change, z: sZ_change});
+      }
+    }
+    if (!undoing) {
+      changes.push(["size", change * -1]);
     }
 }
 
