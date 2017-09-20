402f148390e06601cdd8000e088d0aa9c39715ac
cmullins77 <mullins.cassie@gmail.com>
2017-09-20 14:30:04 -0400

    Creating Home Page

--- controllers/index.js
+++ controllers/index.js
@@ -4,5 +4,8 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.render('index');
 })
+router.get('/home', function(req, res) {
+  res.render('home');
+})
 
 module.exports = router;
