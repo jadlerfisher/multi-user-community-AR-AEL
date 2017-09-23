a6cf00811a7de3e3b4d100142b3f8671f6bf2998
Joshua Wang <joshuawang3110@gmail.com>
2017-09-23 14:21:09 -0400

    Implement Firebase login/register

--- public/js/validations.js
+++ public/js/validations.js
@@ -1,17 +1,4 @@
-function validateRegister(email, password, confirmPassword) {
-  var hasValidated = validateLogin(email, password);
-  var errors = [];
-  var form = document.getElementsByClassName('card-container')[0];
-
-  if (password !== confirmPassword) {
-    errors.push('Confirm password has to match.');
-  }
-
-  displayAlerts(errors, form);
-  return hasValidated && errors.length == 0;
-}
-
-function validateLogin(email, password) {
+function validateUserCredentials(email, password, confirmPassword=null) {
   var alert = document.getElementsByClassName('alert-wrapper');
   if (alert.length) {
     alert[0].remove();
@@ -20,25 +7,32 @@ function validateLogin(email, password) {
   var errors = [];
   var form = document.getElementsByClassName('card-container')[0];
 
-  if (email === '') {
+  if (email) {
+    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+      errors.push('Email is not valid.');
+    }
+  } else {
     errors.push('Email can\'t be empty.');
   }
 
   if (password === '') {
     errors.push('Password can\'t be empty.');
+  } else {
+    if (password.length < 8) {
+      errors.push('Password must be at least 8 characters long.');
+    }
   }
 
-  if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
-    errors.push('Email is not valid.');
-  }
-
-  if (password.length < 8) {
-    errors.push('Password must be at least 8 characters long.');
+  // Validate confirmPassword if the parameter is passed in
+  if (confirmPassword !== null) {
+    if (confirmPassword === '') {
+      errors.push('Confirm password can\'t be empty.');
+    } else if (password !== confirmPassword) {
+      errors.push('Password and confirm password must match.');
+    }
   }
 
   if (errors.length) {
-    wrapper = document.createElement('div');
-    wrapper.className = 'alert-wrapper';
     displayAlerts(errors, form);
   }
 
@@ -46,6 +40,9 @@ function validateLogin(email, password) {
 }
 
 function displayAlerts(errors, form) {
+  var wrapper = document.createElement('div');
+  wrapper.className = 'alert-wrapper';
+
   for (var i = 0; i < errors.length; i++) {
     div = document.createElement('div');
     div.className = 'alert alert-danger';
