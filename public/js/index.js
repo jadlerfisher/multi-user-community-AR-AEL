c60c44801cd37d7f5ab15089174073792b3e9990
Konrad0707 <31453414+Konrad0707@users.noreply.github.com>
2017-10-17 15:49:29 -0400

    Randomize user color

--- public/js/index.js
+++ public/js/index.js
@@ -5,9 +5,11 @@ var itemNum = 0; //The id of the most recently created object
 var models = ["obj: #pokemon-obj", "obj: #cup-obj"]; //The various object models
 var changes = []; //Changes that have been made in editing an object
 var items = []; //List of all the ids of objects in the scene
+var userColor = randomColor();
 
 //Removes the VR button
 function init() {
+    userColor = randomColor();
     document.querySelector("a-scene").removeChild(document.getElementsByClassName("a-enter-vr")[0]);
     buttonExists = false;
 }
@@ -24,7 +26,7 @@ function displayModel(i) {
   model.setAttribute('position', '0 1.25 -5');
   model.setAttribute("rotation", "0 0 0");
   model.setAttribute("scale", "1 1 1");
-  model.setAttribute("material", "color: #0000FF");
+  model.setAttribute("material", "color: " + userColor);
   model.setAttribute("dynamic-body", "mass: 5");
   scene.appendChild(model);
 }
@@ -52,7 +54,7 @@ function display(i) {
     item.setAttribute('VertexA', "0 0 -5");
     item.setAttribute('VertexB', "1 1 -6");
     item.setAttribute('VertexC', "-1 2 -5.5")
-    item.setAttribute('color', "#0000FF");
+    item.setAttribute('color', userColor);
     item.setAttribute("dynamic-body", "mass: 5");
     scene.appendChild(item);
 }
@@ -335,4 +337,13 @@ function removeItem(i) {
   removeButtons();
   createSummonButton();
   createRemoveButton();
+}
+
+//Randomize user color
+function randomColor() {
+ var r = Math.floor(Math.random() * 256);
+ var g = Math.floor(Math.random() * 256);
+ var b = Math.floor(Math.random() * 256);
+ var newCol = "rgb(" + r + ", " + g + ", " + b + ")";
+ return newCol;
 }
