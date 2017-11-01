08f8f65959b93e54af25a02590ea8276ff963126
cmullins77 <mullins.cassie@gmail.com>
2017-11-01 13:07:24 -0400

    Final Updates Before Integration

--- in-progress/app.js
+++ in-progress/app.js
@@ -1,22 +1,144 @@
 //list of lists with class String, function, imagesource, and name for each item
-var galleryList = [["gallery sphere shapes", "myFunct()", "sphere.png", "sphere"],
-                ["gallery cube shapes", "myFunct()", "cube.png", "cube"],
-                ["gallery torus shapes", "myFunct()", "torus.png", "torus"],
-                ["gallery pokeball gaming", "myFunct()", "pokeball.png", "pokeball"],
-                ["gallery cat animals", "myFunct()", "cat.png", "pokeball"],
-                ["gallery dog animals", "myFunct()", "dog.png", "pokeball"],
-                ["gallery hamburger food", "myFunct()", "hamburger.png", "pokeball"],
-                ["gallery ice_cream food", "myFunct()", "ice cream.png", "pokeball"],
-                ["gallery lemon food", "myFunct()", "lemon.jpg", "pokeball"],
-                ["gallery space_invader gaming", "myFunct()", "space invader.png", "pokeball"],
-                ["gallery squirrel animals", "myFunct()", "squirrel.jpg", "pokeball"],
-                ["gallery whale animals", "myFunct()", "whale.png", "pokeball"],
-                ["gallery mario gaming", "myFunct()", "mario.png", "pokeball"]];
+var galleryList = [["gallery sphere shapes", "displayModel(0)", "sphere.png", "sphere"],
+                ["gallery cube shapes", "displayModel(1)", "cube.png", "cube"],
+                ["gallery torus shapes", "displayModel(2)", "torus.png", "torus"],
+                ["gallery pokeball gaming", "displayModel(3)", "pokeball.png", "pokeball"],
+                ["gallery cat animals", "displayModel(4)", "cat.png", "pokeball"],
+                ["gallery dog animals", "displayModel(5)", "dog.png", "pokeball"],
+                ["gallery hamburger food", "displayModel(6)", "hamburger.png", "pokeball"],
+                ["gallery ice_cream food", "displayModel(7)", "ice cream.png", "pokeball"],
+                ["gallery lemon food", "displayModel(8)", "lemon.jpg", "pokeball"],
+                ["gallery space_invader gaming", "displayModel(9)", "space invader.png", "pokeball"],
+                ["gallery squirrel animals", "displayModel(10)", "squirrel.jpg", "pokeball"],
+                ["gallery whale animals", "displayModel(11)", "whale.png", "pokeball"],
+                ["gallery mario gaming", "displayModel(12)", "mario.png", "pokeball"]];
 
 var currentFilter = "gallery";
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+var categories = ['All', 'Shapes', 'Gaming', 'Animals', 'Food'];
+
+function removeAddItemsBox() {
+    document.querySelector("body").removeChild(document.getElementById("uiElem"));
+}
+
+function createAddItemsBox() {
+    var body = document.querySelector("body");
+    body.removeChild(document.getElementById("addButton"))
+        var center1 = document.createElement("center");
+        center1.setAttribute("id", "uiElem");
+
+        var div1 = document.createElement("div");
+        div1.setAttribute("class", "boxAndButtons");
+
+            var div2 = document.createElement("div");
+            div2.setAttribute("class", "objectAddBox");
+
+                var center = document.createElement("center");
+
+                    var div3 = document.createElement("div");
+                    div3.setAttribute("class", "searchbar");
+
+                        var div4 = document.createElement("div");
+                        div4.setAttribute("class", "dropdown");
+
+                            var button = document.createElement("button");
+                            button.setAttribute("class", "dropbtn");
+                            button.innerText = "All";
+
+                            var div5 = document.createElement("div");
+                            div5.setAttribute("id", "myDropdown");
+                            div5.setAttribute("class", "dropdown-content");
+
+                                for (i = 0; i < categories.length; i++) {
+                                    currentCategory = categories[i];
+                                    var a = document.createElement("a");
+                                    a.setAttribute("onclick", "filter('" + currentCategory + "')");
+                                    a.innerText = currentCategory;
+                                    div5.appendChild(a);
+                                }
+                        div4.appendChild(button);
+                        div4.appendChild(div5);
+
+                        var div6 = document.createElement("div");
+                        div6.setAttribute("class", "box");
+
+                            var div7 = document.createElement("div");
+                            div7.setAttribute("class", "container-4");
+
+                                var input = document.createElement("input");
+                                input.setAttribute("onkeypress", "checkKey(event)");
+                                input.setAttribute("type", "search");
+                                input.setAttribute("class", "search");
+                                input.setAttribute("id", "search");
+                                input.setAttribute("placeholder", "Search...");
+
+                                var button2 = document.createElement("button");
+                                button2.setAttribute("onclick", "searchButtonPress()");
+                                button2.setAttribute("class", "icon");
+
+                                    var icon = document.createElement("i");
+                                    icon.setAttribute("class", "fa fa-search");
+
+                                button2.appendChild(icon);
+
+                            div7.appendChild(input);
+                            div7.appendChild(button2);
+
+                        div6.appendChild(div7);
+
+                    div3.appendChild(div4);
+                    div3.appendChild(div6);
+
+
+                    var div8 = document.createElement("div");
+                    div8.setAttribute("class", "objects");
+
+                center.appendChild(div3);
+                center.appendChild(div8);
+
+
+            div2.appendChild(center);
+
+            var button3 = document.createElement("button");
+            button3.setAttribute("class", "uploadButton");
+            button3.innerText = "Upload";
+
+            var button4 = document.createElement("button");
+            button4.setAttribute("class", "cancelButton");
+            button4.setAttribute("onclick", "cancelAddButtonPress()");
+            button4.innerText = "Cancel";
+
+        div1.appendChild(div2);
+        div1.appendChild(button3);
+        div1.appendChild(button4);
+
+    center1.appendChild(div1);
+    body.appendChild(center1);
+
+    displayAllItems();
+}
+
+function cancelAddButtonPress() {
+    removeAddItemsBox();
+    createAddButton();
+}
+function createAddButton() {
+    var button = document.createElement("button");
+    button.setAttribute("id", "addButton");
+    button.setAttribute("onclick", "addButtonPress()");
+    button.innerText = "Add";
+
+    document.querySelector("body").appendChild(button);
+}
+function addButtonPress() {
+    currentFilter = "gallery";
+    createAddItemsBox();
+}
+
+function displayModel(i) {
+    removeAddItemsBox();
+    createAddButton();
+    console.log(i);
 }
 function checkKey(event) {
     var key = event.keyCode;
@@ -43,7 +165,9 @@ function search(searchTerm) {
             var className = currentItem.getAttribute("class");
             var classList = className.split(' ');
             for (j = 1; j< classList.length; j++) {
-                var value = classList[j].indexOf(searchTerm);
+                var currentClass = classList[j];
+                currentClass = currentClass.replace('_',' ');
+                var value = currentClass.indexOf(searchTerm);
                 if (value !== -1) {
                     console.log("MATCH");
                     currentItem.setAttribute("style","display: inline");
