9bc287597ec3ff0a4ae4091ea9351b5474d07c53
cmullins77 <mullins.cassie@gmail.com>
2017-11-01 00:24:38 -0400

    Working on Adding Obj Section
    See progress in in-progress folder

---
+++ in-progress/app.js
@@ -0,0 +1,13 @@
+var options = {
+  valueNames: [ 'name', 'city' ]
+};
+
+var hackerList = new List('hacker-list', options);
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function myFunct() {
+    console.log("CLICKED");
+}
