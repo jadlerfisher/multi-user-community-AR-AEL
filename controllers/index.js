75b5ea45763d9d09b126abd8470f67a8a165daa3
Joshua Wang <joshuawang3110@gmail.com>
2017-11-07 21:09:17 -0500

    Fix login redirect url error

--- controllers/index.js
+++ controllers/index.js
@@ -23,7 +23,10 @@ router.post('/login', function(req, res) {
     	if (error) {
     		return res.status(500).send(error.message);
     	} else {
-    		return res.status(200).send({redirect_url: req.session.redirect_url});
+        var redirect_url = req.session.redirect_url;
+        req.session.redirect_url = '';
+
+    		return res.status(200).send({redirect_url: redirect_url});
       }
     }
   );
