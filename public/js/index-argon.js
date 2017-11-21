b6f20e85796f0b6cfd419ff3f5c1f4c3469e3401
Sean <seanpmcneill@gmail.com>
2017-11-20 22:17:28 -0500

    Testing Cassie's UI with Argon

--- public/js/index-argon.js
+++ public/js/index-argon.js
@@ -1,11 +1,55 @@
-var arScene = document.querySelector('ar-scene');
-var hudElem = document.getElementById('ui');
-var frame = document.querySelector('#frame');
-var hudElem2 = hudElem.cloneNode(true);
-hudElem.id = hudElem.id+"original";
-/* For now, have arScene append HUD at all times
- * For future, arScene only appends when vuforia target is hit
- */
+// // MUST REDEFINE STATE-HANDLER IN HERE
+// var states = ['stateA', ...
+// 							'stateB', ...
+// 							'stateC', ...
+// 							'stateC1', ...
+// 							'stateC2', ...
+// 							'stateC3', ...
+// 							'stateC4'];
+							
+// var currentState = 'stateA';
 
-console.log('This is happening');
-arScene.appendChild(hudElem2);
+// function stateChange(nextState){
+// 	var arScene = document.querySelector("#scene");
+
+// 	console.log('Changing state from:\t' + currentState + "\nChanging state to:\t\t" + nextState);
+	
+
+// 	if(nextState == "stateA"){
+
+// 		var obj = document.getElementsByClassName('selected')[0];
+// 		// remove the "object" as selected
+// 		if(obj !== undefined){
+// 			obj.classList.remove('selected');
+// 		}
+
+// 		// If there is no selected item, automatically hide the option buttons before returning to State A
+// 		if(selectedItem === null){
+// 			hideButtons(document.getElementsByClassName('optionButton'));
+// 		}
+
+// 		// reveal the cursor
+// 		revealCursor();
+// 	}
+
+// 	if(currentState == "stateA"){
+
+// 		// hide cursor
+// 		hideCursor();
+
+// 		// reapply full opacity to selectedItem
+// 		if(selectedItem !== null){
+// 			setOpacity(selectedItem,1);
+// 			selectedItem = null;
+// 		}
+		
+// 	}
+	
+// 	document.getElementById(currentState).classList.add('hide-state');
+// 	document.getElementById(nextState).classList.remove('hide-state');
+
+// 	currentState = nextState;
+// }
+
+// console.log('This is happening');
+// arScene.appendChild(hudElem2);
