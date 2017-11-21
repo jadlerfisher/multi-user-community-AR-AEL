6794eb5f43a4a70f652c7fb577d49cf4ccd8ecef
Sean <seanpmcneill@gmail.com>
2017-11-21 01:09:52 -0500

    trying to add gestures

--- public/js/index-argon.js
+++ public/js/index-argon.js
@@ -1,55 +1,20 @@
-// // MUST REDEFINE STATE-HANDLER IN HERE
-// var states = ['stateA', ...
-// 							'stateB', ...
-// 							'stateC', ...
-// 							'stateC1', ...
-// 							'stateC2', ...
-// 							'stateC3', ...
-// 							'stateC4'];
-							
-// var currentState = 'stateA';
+document.querySelector("#scene").addEventListener('touchstart', function (evt) {
+	var arScene = document.querySelector('#scene');
+	var player = document.querySelector('#player');
+	console.log('The screen was touched');
+	var obj = selectedItem;
+	console.log(obj.id);
+	obj.setAttribute('visible',false);
+	var obj2 = obj.cloneNode('true');
+	obj2.id = obj2.id + '-temp';
 
-// function stateChange(nextState){
-// 	var arScene = document.querySelector("#scene");
+}, false);
 
-// 	console.log('Changing state from:\t' + currentState + "\nChanging state to:\t\t" + nextState);
-	
+document.querySelector("#scene").addEventListener('touchend', function (evt) {
+	console.log('The touch was removed');
+	var arScene = document.querySelector('#scene');
+	var obj = selectedItem;
+	console.log(obj.id);
+	obj.setAttribute('visible',true);
+}, false);
 
-// 	if(nextState == "stateA"){
-
-// 		var obj = document.getElementsByClassName('selected')[0];
-// 		// remove the "object" as selected
-// 		if(obj !== undefined){
-// 			obj.classList.remove('selected');
-// 		}
-
-// 		// If there is no selected item, automatically hide the option buttons before returning to State A
-// 		if(selectedItem === null){
-// 			hideButtons(document.getElementsByClassName('optionButton'));
-// 		}
-
-// 		// reveal the cursor
-// 		revealCursor();
-// 	}
-
-// 	if(currentState == "stateA"){
-
-// 		// hide cursor
-// 		hideCursor();
-
-// 		// reapply full opacity to selectedItem
-// 		if(selectedItem !== null){
-// 			setOpacity(selectedItem,1);
-// 			selectedItem = null;
-// 		}
-		
-// 	}
-	
-// 	document.getElementById(currentState).classList.add('hide-state');
-// 	document.getElementById(nextState).classList.remove('hide-state');
-
-// 	currentState = nextState;
-// }
-
-// console.log('This is happening');
-// arScene.appendChild(hudElem2);
